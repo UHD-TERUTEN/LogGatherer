d18d68d1b1e9b8616746ffbdc0dd2e0245e06edc
droplet92 <lhs1438@naver.com>
2021-01-12 13:04:19 +0900

    설치 완료 시 OS 재시작

--- setup.bat
+++ setup.bat
@@ -6,5 +6,6 @@ echo "2. Register FileAccessMonitor to taskschd..."
 schtasks /create /tn "File Access Monitor" /sc "ONLOGON" /rl HIGHEST /tr "%~dp0%\FileAccessMonitor.exe"
 echo "3. Register send.bat to taskschd..."
 schtasks /create /tn "File Access Log Sender" /sc "DAILY" /st 00:00 /ru system /tr "%~dp0%\send.bat"
-echo "Install Succeeded. It will be automatically closed after 10 seconds..."
+echo "Install Succeeded. OS will automatically restarts after 10 seconds..."
+shutdown -r -t 10
 timeout 10 > NUL
