218cdd55059254e426f8a4bc10aaaae49ae50dc4
droplet92 <lhs1438@naver.com>
2021-04-06 16:50:18 +0900

    AppData\Local 경로 밑에 파일 이동시켜서 사용하도록 변경

--- setup.bat
+++ setup.bat
@@ -3,10 +3,15 @@ echo 1. Copy DetoursLog.dll to %WINDIR%\System32...
 set SOURCE_PATH=%~dp0%DetoursLog.dll
 set TARGET_PATH=%WINDIR%\System32\DetoursLog.dll
 copy "%SOURCE_PATH%" "%TARGET_PATH%"
-echo 2. Register FileAccessMonitor to taskschd...
-schtasks /create /tn "File Access Monitor" /sc "ONLOGON" /rl HIGHEST /tr "%~dp0%\FileAccessMonitor.exe"
-echo 3. Register send.bat to taskschd...
-schtasks /create /tn "File Access Log Sender" /sc "DAILY" /st 00:00 /ru system /tr "%~dp0%\send.bat"
+setx LOGGATHERERROOT "%LOCALAPPDATA%\LogGatherer"
+echo 2. Copy Files to %LOGGATHERERROOT%...
+mkdir "%LOGGATHERERROOT%\Logs"
+set SOURCE=%~dp0%*
+copy "%SOURCE%" "%LOGGATHERERROOT%"
+echo 3. Register FileAccessMonitor to taskschd...
+schtasks /create /tn "File Access Monitor" /sc "ONLOGON" /rl HIGHEST /tr "%LOGGATHERERROOT%\FileAccessMonitor.exe"
+echo 4. Register send.bat to taskschd...
+schtasks /create /tn "File Access Log Sender" /sc "DAILY" /st 00:00 /ru system /tr "%LOGGATHERERROOT%\send.bat"
 echo Install Succeeded. OS will automatically restarts after 10 seconds..."
 shutdown -r -t 10
 timeout 10 > NUL
