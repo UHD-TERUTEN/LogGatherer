996a88384dbdf76a3554f2d80bbfd9b8f538d48a
droplet92 <lhs1438@naver.com>
2021-04-08 01:50:29 +0900

    전송 후 압축 파일 삭제

--- send.bat
+++ send.bat
@@ -7,10 +7,11 @@ echo start update
 echo zipping log files
 tar -cf "%DATE%.zip" -C %LogGathererRoot%\Logs\ .
 echo.
-echo uploading zip
+echo uploading zip file
 echo ssh connecting to %ssh_account%...
 ssh -i %id_rsa_location% %ssh_account% "mkdir logs"
 scp -i %id_rsa_location% "%DATE%.zip" "%ssh_account%:logs"
+del "%DATE%.zip"
 echo.
 echo log files have been uploaded!
 echo finish
