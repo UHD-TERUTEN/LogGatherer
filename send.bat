413e9fbf01a04d1f531bf63150ad77be08e3de4e
droplet92 <lhs1438@naver.com>
2021-05-07 14:05:37 +0900

    zip 압축 시 해제 안되는 문제로 tar.gz 압축으로 변경, 전송한 로그 삭제

--- send.bat
+++ send.bat
@@ -1,17 +1,19 @@
 @echo off
-for /F "tokens=* USEBACKQ" %%F in (account.txt) do (
+for /F "tokens=* USEBACKQ" %%F in (%LogGathererRoot%\account.txt) do (
 set ssh_account=%%F
 )
 set id_rsa_location=%HOMEDRIVE%%HOMEPATH%\.ssh\id_rsa
 echo start update
 echo zipping log files
-tar -cf "%DATE%.zip" -C %LogGathererRoot%\Logs\ .
+mkdir "%LogGathererRoot%\Logs\%DATE%"
+move %LogGathererRoot%\Logs\*.txt "%LogGathererRoot%\Logs\%DATE%"
+tar -zcvf "%LogGathererRoot%\Logs\%DATE%\%DATE%.tar.gz" -C %LogGathererRoot%\Logs\%Date% .
 echo.
 echo uploading zip file
 echo ssh connecting to %ssh_account%...
 ssh -i %id_rsa_location% %ssh_account% "mkdir logs"
-scp -i %id_rsa_location% "%DATE%.zip" "%ssh_account%:logs"
-del "%DATE%.zip"
+scp -i %id_rsa_location% "%LogGathererRoot%\Logs\%DATE%\%DATE%.tar.gz" "%ssh_account%:logs"
+rmdir /s /q "%LogGathererRoot%\Logs\%DATE%"
 echo.
 echo log files have been uploaded!
 echo finish
