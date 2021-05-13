9026ca47b0bf4ae1d0c973fca854f3bd7ca276c1
droplet92 <lhs1438@naver.com>
2021-05-13 18:10:30 +0900

    32-bit 및 64-bit 모두 대응

--- setup.bat
+++ setup.bat
@@ -1,7 +1,10 @@
 @echo off
 echo 1. Copy DetoursLog.dll to %WINDIR%\System32...
-set SOURCE_PATH=%~dp0%DetoursLog.dll
-set TARGET_PATH=%WINDIR%\System32\DetoursLog.dll
+set SOURCE_PATH=%~dp0%DetoursLog*.dll
+set TARGET_PATH=%WINDIR%\System32\
+copy "%SOURCE_PATH%" "%TARGET_PATH%"
+set SOURCE_PATH=%~dp0%DetoursLog64.dll
+set TARGET_PATH=%WINDIR%\SysWOW64\
 copy "%SOURCE_PATH%" "%TARGET_PATH%"
 setx /M LogGathererRoot "%LOCALAPPDATA%\LogGatherer"
 echo 2. Copy Files to %LogGathererRoot%...
@@ -9,7 +12,8 @@ mkdir "%LogGathererRoot%\Logs"
 set SOURCE=%~dp0%*
 copy "%SOURCE%" "%LogGathererRoot%"
 echo 3. Register FileAccessMonitor to taskschd...
-schtasks /create /tn "File Access Monitor" /sc "ONLOGON" /rl HIGHEST /tr "%LogGathererRoot%\FileAccessMonitor.exe"
+schtasks /create /tn "File Access Monitor 32-bit" /sc "ONLOGON" /rl HIGHEST /tr "%LogGathererRoot%\FileAccessMonitor32.exe"
+schtasks /create /tn "File Access Monitor 64-bit" /sc "ONLOGON" /rl HIGHEST /tr "%LogGathererRoot%\FileAccessMonitor64.exe"
 echo 4. Register send.bat to taskschd...
 schtasks /create /tn "File Access Log Sender" /sc "DAILY" /rl HIGHEST /st 12:00 /tr "%LogGathererRoot%\send.bat"
 echo Install Succeeded. OS will automatically restarts after 10 seconds..."
