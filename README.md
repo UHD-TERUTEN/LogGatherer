2c82b387934e95ea1554a166d93257f466e5252f
droplet92 <lhs1438@naver.com>
2021-05-20 02:46:18 +0900

    - 환경 변수 설정하기 전에 출력하는 부분 변경 - 테스트 시나리오에 맞춰 오후 1시에 로그 전송하게 변경 - README 업데이트

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 학습용 로그 수집 서브시스템 
 
 1. Windows 계정에 로그온하면 FileAccessMonitor 실행됩니다.
-2. 이 상태로 프로그램을 켜면 프로세스에 dll injection을 실행하고,   
-해당 프로그램에서 발생한 파일 접근 기록을 D:\log.txt에 로깅합니다.
-3. 매일 오전 0시에 account.txt에 설정된 서버로 해당 로그를 전송합니다.
+2. 이 상태로 프로그램을 켜면 프로세스에 DLL injection을 실행하고,   
+해당 프로그램에서 발생한 파일 접근 기록을 %LOCALAPPDATA%\LogGatherer\Logs\<프로그램이름>.exe.txt에 로깅합니다.
+3. 매일 오후 1시에 account.txt에 설정된 서버로 해당 로그를 전송합니다.
 
 ### 설치
 - 압축 해제 후 setup.bat을 관리자 권한으로 실행
@@ -17,4 +17,4 @@
 - 매일 오전 0시 정각 (setup.bat 파일이나 taskschd에서 직접 수정)
 
 ### 프로모션 영상
-[![학습용 로그 수집 서브시스템 설치 영상](https://i9.ytimg.com/vi/3Tyo85Gf3Yo/mq2.jpg?sqp=CLj69P8F&rs=AOn4CLAVodshwRBxfe_nMEQPpb7FkAHKHg)](https://www.youtube.com/watch?v=3Tyo85Gf3Yo)
+[![학습용 로그 수집 서브시스템 설치 영상](https://i9.ytimg.com/vi/3Tyo85Gf3Yo/mq2.jpg?sqp=CJSPlYUG&rs=AOn4CLAnOx_r7MD1iuITnquHaiPHYpiRmg)](https://www.youtube.com/watch?v=3Tyo85Gf3Yo)
--- setup.bat
+++ setup.bat
@@ -6,16 +6,16 @@ copy "%SOURCE_PATH%" "%TARGET_PATH%"
 set SOURCE_PATH=%~dp0%DetoursLog32.dll
 set TARGET_PATH=%WINDIR%\SysWOW64\
 copy "%SOURCE_PATH%" "%TARGET_PATH%"
-echo 2. Copy Files to %LogGathererRoot%...
-mkdir "%LOCALAPPDATA%\LogGatherer\Logs"
 setx /M LogGathererRoot "%LOCALAPPDATA%\LogGatherer"
-set SOURCE=%~dp0%*
-copy "%SOURCE%" "%LogGathererRoot%"
+echo 2. Copy Files to %LogGathererRoot%...
+mkdir "%LogGathererRoot%\Logs"
+set SOURCE_PATH=%~dp0%*
+copy "%SOURCE_PATH%" "%LogGathererRoot%"
 echo 3. Register FileAccessMonitor to taskschd...
 schtasks /create /tn "File Access Monitor 32-bit" /sc "ONLOGON" /rl HIGHEST /tr "%LogGathererRoot%\FileAccessMonitor32.exe"
 schtasks /create /tn "File Access Monitor 64-bit" /sc "ONLOGON" /rl HIGHEST /tr "%LogGathererRoot%\FileAccessMonitor64.exe"
 echo 4. Register send.bat to taskschd...
-schtasks /create /tn "File Access Log Sender" /sc "DAILY" /rl HIGHEST /st 12:00 /tr "%LogGathererRoot%\send.bat"
+schtasks /create /tn "File Access Log Sender" /sc "DAILY" /rl HIGHEST /st 13:00 /tr "%LogGathererRoot%\send.bat"
 echo Install Succeeded. OS will automatically restarts after 10 seconds..."
 shutdown -r -t 10
 timeout 10 > NUL
